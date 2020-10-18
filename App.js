e01ed9c754a30dd65b7f3f6253006c0593ffa4ab
hari-mothukuri <hari.mothukuri@vwcredit.com>
2020-10-17 22:29:40 -0700

    AWS Image Rekognition using Expo React Native App

--- App.js
+++ App.js
@@ -1,14 +1,144 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { Root, Container, Button, Content, Text, ActionSheet, H2, Card, CardItem, Body } from 'native-base';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+var BUTTONS = [
+  { text: "Choose Photo", icon: "american-football", iconColor: "#2c8ef4" },
+  { text: "Take Photo", icon: "analytics", iconColor: "#f42ced" },
+  { text: "Cancel", icon: "close", iconColor: "#25de5b" }
+];
+var CANCEL_INDEX = 2;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const aws = require('aws-sdk');
+import { Buffer } from 'buffer'
+import * as FileSystem from 'expo-file-system';
+var rekognition = new aws.Rekognition();
+import AWS_CONFIG from './aws-config';
+console.log(AWS_CONFIG);
+aws.config.update(AWS_CONFIG);
+
+export default class App extends React.Component {
+
+  //Reads file from local file system
+  async readFile(file) {
+    return await FileSystem.readAsStringAsync(file, { encoding: 'base64' });
+  }
+
+  state = {
+    loading: false,
+    detectedTexts: []
+  };
+
+  recognizeTextFromImage = async (path) => {
+    console.log('In recognizeTextFromImage()');
+    this.readFile(path).then((data) => {
+      const buffer = new Buffer(data, 'base64');
+      rekognition.detectText({
+        Image: {
+          Bytes: buffer
+        }
+      }).promise()
+        .then((res) => {
+          var detectedTexts = [];
+          const map1 = res.TextDetections.map((x) => {
+            detectedTexts.push(x.DetectedText);
+          }
+          );
+          this.setState({
+            isLoading: false,
+            detectedTexts: detectedTexts
+          });
+        });
+
+    })
+      .catch(error => {
+        console.error(error);
+      });
+
+  };
+  detectText = () => {
+    ActionSheet.show(
+      {
+        options: BUTTONS,
+        cancelButtonIndex: CANCEL_INDEX
+      },
+      buttonIndex => {
+        if (buttonIndex == 0)
+          this._pickImage();
+        if (buttonIndex == 1)
+          this._takePhoto();
+      }
+    );
+  }
+  _takePhoto = async () => {
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      base64: true,
+      allowsEditing: true,
+      aspect: [4, 3],
+      allowsMultipleSelection: true
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _pickImage = async () => {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      base64: true,
+      allowsEditing: true,
+      aspect: [4, 3],
+      allowsMultipleSelection: true
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+  _handleImagePicked = async pickerResult => {
+    try {
+      this.setState({ loading: true });
+      if (!pickerResult.cancelled) {
+        await this.recognizeTextFromImage(pickerResult.uri);
+      }
+    } catch (e) {
+      console.log(e);
+      alert('Error in handling image picker :(');
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    return (
+      <Root>
+        <Container>
+          <Content padder>
+            <Card>
+              <CardItem header bordered>
+                <Text>Detected Texts</Text>
+              </CardItem>
+              <CardItem bordered>
+                <Body>
+                  {
+                    this.state.detectedTexts.map((text, i) => (
+                      <Text key={i}>{text}</Text>
+                    ))
+                  }
+                </Body>
+              </CardItem>
+              <CardItem footer bordered>
+                <Button style={{ alignSelf: "center" }} success block onPress={this.detectText}>
+                  <Text>SCAN PHOTO</Text>
+                </Button>
+                {
+                  this.state.loading ?
+                    <ActivityIndicator size="small" color={'black'} /> : null
+                }
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
+      </Root>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ aws-config.js
@@ -0,0 +1,8 @@
+export default AWS_CONFIG = {
+    region: 'us-west-1',
+    credentials: {
+        accessKeyId: '<Your AWS Access Key>',
+        secretAccessKey: '<Your AWS Secret Access Key>'
+    }
+};
+
